6681e43cab81ad80aad545e8c5553022114edd86
hiroaki <hark.arkw@gmail.com>
2020-07-08 11:40:03 +0900

    immplement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React, { useState } from "react"
 
-const  App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count -1)
-  const double = () => setCount(count *2)
-  const reset = () => setCount(0)
-  const increment2 = () => setCount(previousCount => previousCount +1 )
-  const decrement2 = () => setCount(previousCount => previousCount -1 )
-  const devide3 = () => setCount(previousCount => 
-    previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  )
+const  App = props => {  
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        関数を使ったsetCount<br />
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={double}>x2</button>
-      </div>
-      <div>
-        <button onClick={devide3}>3の倍数は3で割る</button>
-      </div>
-      <div>
-      <button onClick={reset}>Reset</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={()=> setPrice(price +1)}>+1</button>
+      <button onClick={()=> setPrice(price -1)}>-1</button>
+      <button onClick={reset}>Reset</button><br />
+      商品：<input value={name} onChange={e => setName(e.target.value)} />
     </>
   )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
