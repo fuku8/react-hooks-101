bdb2fc0ac1229cd4ba041e2ea99d306577ba5eac
hiroaki <hark.arkw@gmail.com>
2020-07-11 13:58:16 +0900

    create one React Context

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
+import { DELETE_EVENT } from './actions'
+
 const Event = ({ dispatch, event }) => {
     const id =event.id
     const handleClickDeleteButton = () =>{
       const result = window.confirm(`イベント（id=${id}）を本当に削除しても良いですか？`)
-      const DELETE_EVENT = 'DELETE_EVENT'
       if (result) dispatch({ type: DELETE_EVENT, id })
     }
 
